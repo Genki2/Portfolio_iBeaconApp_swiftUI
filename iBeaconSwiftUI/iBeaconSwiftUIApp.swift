8d7d3d614f4d8ab4be36c47a7f93bf0a4ee54303
GENKI2 <genki.fujimoto@tangerine.io>
2022-12-02 17:33:00 +0900

    Initial Commit

---
+++ iBeaconSwiftUI/iBeaconSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBeaconSwiftUIApp.swift
+//  iBeaconSwiftUI
+//
+//  Created by GENKIFUJIMOTO on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct iBeaconSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
